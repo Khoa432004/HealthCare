b10e0c40bca6c1b5104a67f21c663c7217f39c9d
Khoa <157439923+Khoa432004@users.noreply.github.com>
2025-10-29 16:24:59 +0700

    first commit

--- HealthCare/src/main/java/com/example/HealthCare/exception/UsernameIsExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.HealthCare.exception;
-
-public class UsernameIsExistException extends RuntimeException {
-	public UsernameIsExistException(String message) {
-		super(message);
-	}
-}
