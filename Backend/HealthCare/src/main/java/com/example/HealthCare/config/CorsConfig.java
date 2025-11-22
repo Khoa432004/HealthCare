a720fd0b3147a314a24b22f8efdc49b3284c052f
nguyenvankhanh2010 <vkhanh1224@gmail.com>
2025-11-22 08:41:00 +0700

    VNPay

--- Backend/HealthCare/src/main/java/com/example/HealthCare/config/CorsConfig.java
+++ Backend/HealthCare/src/main/java/com/example/HealthCare/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         
         // Allow frontend origins
-        configuration.setAllowedOrigins(Arrays.asList(
+        configuration.setAllowedOriginPatterns(Arrays.asList(
             " http://localhost:3000",
             "http://127.0.0.1:3000",
             "https://localhost:3000"
