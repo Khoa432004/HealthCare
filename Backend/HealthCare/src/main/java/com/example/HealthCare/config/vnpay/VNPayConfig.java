72f4ba69172b961ac9fe7555d71a26cca7a6fd71
lucasvo184 <thanhlocth1tvt@gmail.com>
2026-04-28 11:46:07 +0700

    config: VNPay key congig

--- Backend/HealthCare/src/main/java/com/example/HealthCare/config/vnpay/VNPayConfig.java
+++ Backend/HealthCare/src/main/java/com/example/HealthCare/config/vnpay/VNPayConfig.java
@@ -14,10 +14,11 @@ import java.util.*;
 @Component
 public class VNPayConfig {
 public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-public static String vnp_Returnurl = "/vnpay-payment"; 
-public static String vnp_TmnCode = "FXXUNF0W";
-public static String vnp_HashSecret = "S99ZV9P0AHUACFTR8RXHEJKQVBX4RK13";
+public static String vnp_Returnurl = "http://localhost:8080/vnpay-payment";
+public static String vnp_TmnCode = "YTECH001";
+public static String vnp_HashSecret = "ZKQAEMJJMTFGYGHCJQSKZTYJIWWIZPRI";
 public static String vnp_apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
+public static String vnp_IpnUrl = "https://nonoptimistic-unannounced-delilah.ngrok-free.dev/api/vnpay/ipn";
 
     public static String md5(String message) {
         String digest = null;
